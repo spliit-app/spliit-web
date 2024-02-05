1c960be07883c33221da7c53ecddabddcbec036a
Sebastien Castiel <sebastien@castiel.me>
2024-02-04 22:29:24 -0500

    Add FAQ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 import { Contributors } from '@/app/contributors'
 import { StatsDisplay } from '@/app/stats-display'
+import { FeedbackModal } from '@/components/feedback-button/feedback-button'
 import { TrackPage } from '@/components/track-page'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion'
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
+import { env } from '@/lib/env'
 import {
   BarChartHorizontalBig,
   CircleDollarSign,
@@ -149,6 +157,133 @@ export default function HomePage() {
           </div>
         </div>
       </section>
+
+      <section className="bg-slate-50 dark:bg-card py-16 md:py-24 lg:py-32">
+        <div className="p-4 flex mx-auto max-w-screen-md flex-col items-center text-center">
+          <h2 className="font-bold text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
+            Frequently Asked Questions
+          </h2>
+          <div className="text-left px-4 flex flex-col gap-4 max-w-screen-md mx-auto w-full mt-8">
+            <Accordion type="multiple">
+              <Answer
+                id="free-to-use"
+                question={
+                  <>
+                    Is <strong>Spliit</strong> free to use?
+                  </>
+                }
+              >
+                Yes, you can use Spliit for free, without any limitation! Note
+                that in the future, we might add premium features, but we’ll
+                never put essential features behind a paywall.
+              </Answer>
+              <Answer
+                id="differences"
+                question={
+                  <>
+                    What differentiates <strong>Spliit</strong> from other
+                    similar services?
+                  </>
+                }
+              >
+                Spliit is more minimalist than Splitwise or Tricount, and you
+                don’t need to create any user account to use it, nor will you
+                see any ads. It offers similar features, but we’re still working
+                on some of them. Have a look at{' '}
+                <a
+                  target="_blank"
+                  href="https://github.com/spliit-app/spliit/issues"
+                >
+                  issues on GitHub
+                </a>{' '}
+                to have an idea of what our contributors are working on!
+              </Answer>
+              <Answer id="data" question={<>How is my data stored?</>}>
+                All the data you enter on Spliit (groups, expenses…) is stored
+                in a PostgreSQL database hosted by{' '}
+                <a target="_blank" href="https://vercel.com">
+                  Vercel
+                </a>{' '}
+                (same as the web application itself). For now, the data is not
+                encrypted, but we’re trying to find the best way to add
+                encryption without impacting the user experience too much (see{' '}
+                <a
+                  target="_blank"
+                  href="https://github.com/spliit-app/spliit/issues/34"
+                >
+                  this issue on GitHub
+                </a>
+                ).
+              </Answer>
+              <Answer
+                id="feedback"
+                question={
+                  <>
+                    What is the best way to give feedback or suggest a feature?
+                  </>
+                }
+              >
+                You can give us feedback by using{' '}
+                <FeedbackModal donationUrl={env.STRIPE_DONATION_LINK}>
+                  <Button variant="link" className="text-base -mx-4 -my-4">
+                    our feedback form
+                  </Button>
+                </FeedbackModal>
+                . We’ll receive it by email and will keep you update, if you
+                want to provide your email. But even better, if you know GitHub
+                and have an account, you can report issues or suggest a feature
+                by{' '}
+                <a
+                  target="_blank"
+                  href="https://github.com/spliit-app/spliit/issues"
+                >
+                  creating an issue
+                </a>{' '}
+                there. This way, all our contributors will see it and will be
+                able to give their insight.
+              </Answer>
+              <Answer
+                id="contribute"
+                question={<>I use Spliit and like it. How can I contribute?</>}
+              >
+                <p>You can contribute to Spliit by several ways.</p>
+                <ul>
+                  <li>
+                    You can share it with your community on social media to let
+                    them know about us,
+                  </li>
+                  <li>
+                    You can{' '}
+                    <FeedbackModal
+                      donationUrl={env.STRIPE_DONATION_LINK}
+                      defaultTab="support"
+                    >
+                      <Button
+                        variant="link"
+                        className="text-base text-pink-600 -mx-4 -my-4"
+                      >
+                        support our hosting costs
+                      </Button>
+                    </FeedbackModal>{' '}
+                    by sponsoring us or donating a small amount,
+                  </li>
+                  <li>
+                    If you’re a developer, you can implement new features or
+                    improve the user experience! Go to{' '}
+                    <a
+                      target="_blank"
+                      href="https://github.com/spliit-app/spliit"
+                    >
+                      our GitHub repository
+                    </a>{' '}
+                    to know more about the project.
+                  </li>
+                </ul>
+              </Answer>
+            </Accordion>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
@@ -182,5 +317,26 @@ function Feature({
         {description}
       </div>
     </div>
+  )
+}
+
+function Answer({
+  id,
+  question,
+  children,
+}: {
+  id: string
+  question: ReactNode
+  children: ReactNode
+}) {
+  return (
+    <AccordionItem value={id}>
+      <AccordionTrigger className="text-left text-lg">
+        <span>{question}</span>
+      </AccordionTrigger>
+      <AccordionContent className="prose dark:prose-invert">
+        {children}
+      </AccordionContent>
+    </AccordionItem>
   )
 }
